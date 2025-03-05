663b0cadbffeacfb13240f9b99796a269cca454a
luk4szm <lukasz@mikowski.pl>
2025-03-05 15:03:39 +0100

    front init

---
+++ src/Controller/Front/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Service\DeviceStatus\DeviceStatusHelperInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class DashboardController extends AbstractController
+{
+    #[Route('/', name: 'app_front_dashboard')]
+    public function index(
+        #[AutowireIterator('app.shelly.device_status_helper')]
+        iterable $statusHelpers,
+    ): Response
+    {
+        /** @var DeviceStatusHelperInterface $helper */
+        foreach ($statusHelpers as $helper) {
+            $devices[$helper->getDeviceName()] = $helper->getHistory();
+        }
+
+        return $this->render('front/dashboard/index.html.twig', [
+            'devices' => $devices ?? [],
+        ]);
+    }
+}
