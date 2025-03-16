576160444aed36619da1d6a6e539ba3d16a92a6e
luk4szm <lukasz@mikowski.pl>
2025-03-16 09:51:55 +0100

    autoupdate device temp row

--- public/js/refresh_temperatures_card.js
+++ public/js/refresh_temperatures_card.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    setInterval(refreshTemperatureCard, 20000);
+    setInterval(refreshTemperatureCard, 30000);
 
     function refreshTemperatureCard() {
         $('#temperatures_card').load(location.href + " " + '#temperatures_card');
