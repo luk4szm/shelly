e5e464b6256ab4542d5960bddde811aaca473689
luk4szm <lukasz@mikowski.pl>
2025-03-21 18:09:57 +0100

    send h&t report on mail p2

--- src/Controller/Hook/HookController.php
+++ src/Controller/Hook/HookController.php
@@ -5,21 +5,22 @@ namespace App\Controller\Hook;
 use App\Entity\Hook;
 use App\Repository\HookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HookController extends AbstractController
 {
     #[Route('/hook/{device}/{property}/{value}', name: 'app_hoke_save')]
-    public function hook(string $device, string $property, string $value, HookRepository $repository): Response
+    public function hook(Request $request, string $device, string $property, string $value, HookRepository $repository): Response
     {
         $hook = new Hook($device, $property, $value);
 
         $repository->save($hook);
 
         // temp
-        if ($device === 'sypialnia') {
-            mail('lukasz@mikowski.pl', 'shelly h&t report', $value);
+        if (in_array($device, ['salon', 'sypialnia', 'stas', 'dziewczynki'], true)) {
+            mail('lukasz@mikowski.pl', 'shelly h&t report', $request->getContent());
         }
 
         return $this->json($hook);
