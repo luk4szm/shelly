1cc591eb7d111e82955352ac07bdcc10b92bc972
lukasz <lukasz@mikowski.pl>
2025-03-27 14:56:49 +0100

    iffi update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-git pull -f
+if git pull -f | grep -q "Already up to date."; then
+  echo "Repo aktualne!"
+  exit 0
+fi
+
 php bin/console d:s:u --dump-sql --force
 php bin/console cache:clear
