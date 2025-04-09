146108df670cef2b3f9a191556227cef6c65ca6b
luk4szm <lukasz@mikowski.pl>
2025-04-10 00:20:35 +0200

    handle touchend event

--- public/js/supla_gate_opener.js
+++ public/js/supla_gate_opener.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#gate_opener_range_input').on('mouseup', function () {
+    $('#gate_opener_range_input').on('mouseup touchend', function () {
         var currentValue = parseInt($(this).val());
 
         if (currentValue === 100) {
