6c75f8ebedc46b816172f197605a0e2e231af9eb
luk4szm <lukasz@mikowski.pl>
2025-04-11 00:01:45 +0200

    gate opening info status

--- public/js/supla_gate_opener.js
+++ public/js/supla_gate_opener.js
@@ -4,17 +4,24 @@ $(document).ready(function () {
         behavior: 'smooth'
     });
 
-    $('#gate_opener_range_input').on('mouseup touchend', function () {
-        var currentValue = parseInt($(this).val());
+    let infoButton = $("#gate_opener_info_button_div")
+    let progressBar = $("#gate_opener_progress_bar");
+    let rangeInput = $('#gate_opener_range_input');
+
+    rangeInput.on('mouseup touchend', function () {
+        let currentValue = parseInt($(this).val());
 
         if (currentValue === 100) {
+            hideDiv(rangeInput);
+            showDiv(infoButton);
+
             $.ajax({
                 url: '/supla/gate/open',
                 method: 'PATCH',
-                success: function (response) {
-                    console.log(response);
-
-                    $(this).val(0);
+                success: function () {
+                    $("#gate_opener_info_button").html('Gate is opening...');
+                    showDiv(progressBar);
+                    showGateOpeningProgress(progressBar);
                 },
             });
         } else {
@@ -22,3 +29,39 @@ $(document).ready(function () {
         }
     });
 });
+
+function showDiv(selector)
+{
+    selector
+        .removeClass('d-none')
+        .addClass('d-grid')
+        .css({opacity: 0}) // Ustaw początkowy stan dla animacji
+        .animate({opacity: 1}, 1000, function () {
+            // Opcjonalnie: kod do wykonania po zakończeniu animacji
+        });
+}
+
+function hideDiv(selector)
+{
+    selector
+        .removeClass('d-grid')
+        .addClass('d-none');
+}
+
+function showGateOpeningProgress() {
+    let duration = 18000; // 18 sekund w milisekundach
+    let progressBar = $(".progress-bar");
+
+    progressBar.animate({
+        width: '100%'
+    }, duration, function () {
+        $("#gate_opener_info_button")
+            .removeClass('btn-primary')
+            .addClass('btn-success')
+            .html('Gate open!');
+
+        $("#gate_opener_info_button_div").removeClass('mb-2');
+
+        hideDiv($("#gate_opener_progress_bar"));
+    });
+}
