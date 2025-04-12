05a83b0d7ac9aa795d222bb729951b52c9c32317
luk4szm <lukasz@mikowski.pl>
2025-04-12 22:22:34 +0200

    supla gate opener logger

--- src/Service/Gate/SuplaGateOpener.php
+++ src/Service/Gate/SuplaGateOpener.php
@@ -3,50 +3,105 @@
 namespace App\Service\Gate;
 
 use App\Service\Curl\Supla\SuplaCloudCurlRequest;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 readonly class SuplaGateOpener
 {
+    private const SLEEP_TIME = 2;
+    private ?UserInterface $user;
+
     public function __construct(
         private SuplaCloudCurlRequest $suplaCurl,
+        private LoggerInterface       $gateOpenerLogger,
+        private Security              $security,
     ) {
     }
 
     public function open(): bool
     {
-        if ($this->isOpen())
-        {
-//            dump('gate is already open');
-            return true;
-        }
-
-//        dump('sending open 1nd open request');
-        $this->suplaCurl->openClose();
-
-        sleep(2);
+        $this->user = $this->security->getUser();
 
         if ($this->isOpen())
         {
-//            dump('gate is opening by 1st request');
+            $this->gateOpenerLogger->info(
+                'Request refused. Gate is already open',
+                ['user' => $this->user?->getUserIdentifier()],
+            );
+
             return true;
         }
 
-//        dump('sending open 2nd open request');
-        $this->suplaCurl->openClose();
+        $this->tryOpen();
 
-        sleep(2);
+        sleep(self::SLEEP_TIME);
 
         if ($this->isOpen())
         {
-//            dump('gate is opening by 2nd request');
+            $this->gateOpenerLogger->info(
+                'Gate is opening by 1st request',
+                ['user' => $this->user?->getUserIdentifier()],
+            );
+
             return true;
         }
 
-        return $this->isOpen();
+        $this->tryOpen();
+
+        sleep(self::SLEEP_TIME);
+
+        if ($this->isOpen())
+        {
+            $this->gateOpenerLogger->info(
+                'Gate is opening by 2nd request',
+                ['user' => $this->user?->getUserIdentifier()],
+            );
+
+            return true;
+        }
+
+        $this->gateOpenerLogger->error(
+            'Error occurred. Cannot open gate',
+            ['user' => $this->user?->getUserIdentifier()],
+        );
+
+        return false;
+    }
+
+    public function read(): array
+    {
+        return $this->suplaCurl->read();
+    }
+
+    private function tryOpen(): bool
+    {
+        $status = $this->suplaCurl->openClose();
+
+        if (!isset($status['success']) || $status['success'] === false) {
+            $this->gateOpenerLogger->error(
+                'Couldn\'t open gate',
+                ['user' => $this->user?->getUserIdentifier()],
+            );
+
+            throw new \Exception('Couldn\'t open gate');
+        }
+
+        return true;
     }
 
     private function isOpen(): bool
     {
         $status = $this->suplaCurl->read();
+
+        if (!isset($status['connected']) || $status['connected'] === false) {
+            $this->gateOpenerLogger->error(
+                'Couldn\'t connect with Supla',
+                ['user' => $this->user?->getUserIdentifier()],
+            );
+
+            throw new \Exception('Couldn\'t connect with Supla');
+        }
 
         return !$status['hi'];
     }
