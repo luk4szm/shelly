ce277dde1cc1d0417c474c05852a1575c7459d95
miko <lukasz@mikowski.pl>
2025-06-02 17:38:28 +0200

    cover controller logger

--- src/Service/Shelly/Cover/ShellyCoverService.php
+++ src/Service/Shelly/Cover/ShellyCoverService.php
@@ -2,14 +2,34 @@
 
 namespace App\Service\Shelly\Cover;
 
+use App\Service\Curl\Shelly\ShellyCloudCurlRequest;
 use App\Service\Shelly\ShellyDeviceService;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 
 readonly class ShellyCoverService extends ShellyDeviceService
 {
     private const SHELLY_DEVICE_ID = '2CBCBB2DC408';
 
+    public function __construct(
+        ShellyCloudCurlRequest  $curlRequest,
+        private LoggerInterface $coverControllerLogger,
+        private Security        $security,
+    )
+    {
+        parent::__construct($curlRequest);
+    }
+
     public function open(): array
     {
+        $this->coverControllerLogger->info(
+            'Covers have been opened',
+            [
+                'user'   => $this->security->getUser()?->getUserIdentifier(),
+                'device' => 'app',
+            ],
+        );
+
         $this->curlRequest->cover(self::SHELLY_DEVICE_ID, 'open');
 
         sleep(25);
@@ -19,6 +39,14 @@ readonly class ShellyCoverService extends ShellyDeviceService
 
     public function close(): array
     {
+        $this->coverControllerLogger->info(
+            'Covers have been closed',
+            [
+                'user'   => $this->security->getUser()?->getUserIdentifier(),
+                'device' => 'app',
+            ],
+        );
+
         return $this->curlRequest->cover(self::SHELLY_DEVICE_ID, 'close');
     }
 
