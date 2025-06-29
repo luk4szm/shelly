19cd0e0860c2962c2b5bd399e49850214d56552a
miko <lukasz@mikowski.pl>
2025-06-29 22:57:51 +0200

    garage, cover controller js refactor

--- public/js/collapse_controllers.js
+++ public/js/collapse_controllers.js
@@ -12,74 +12,60 @@ $(document).ready(function () {
         }
     });
 
-    $('#cover-control button[data-role]').on('click', function () {
-        let clickedButton = $(this);
-        let direction = clickedButton.data('role');
-
-        $.ajax({
-            type: "PATCH",
-            url: "/cover/open-close",
-            data: {"direction": direction},
-            success: function () {
-                setTimeout(function () {
-                    let collapseElement = clickedButton.closest('.accordion-collapse')[0];
-
-                    if (collapseElement) {
-                        let bsCollapse = bootstrap.Collapse.getInstance(collapseElement);
-
-                        if (bsCollapse) {
-                            bsCollapse.hide();
-                        } else {
-                            new bootstrap.Collapse(collapseElement).hide();
-                        }
-                    }
-                }, 1000);
-            },
-            error: function (response) {
-                const toastEl = document.getElementById('liveToast');
-                const liveToast = new bootstrap.Toast(toastEl, {});
-
-                $(".toast-body").html(response.responseJSON);
-
-                liveToast.show();
-            }
-        });
-    });
-
-    $('#garage-control button[data-role]').on('click', function () {
-        let clickedButton = $(this);
-        let direction = clickedButton.data('role');
-
-        $.ajax({
-            type: "PATCH",
-            url: "/garage/move",
-            data: {"direction": direction},
-            success: function () {
-                setTimeout(function () {
-                    let collapseElement = clickedButton.closest('.accordion-collapse')[0];
-
-                    if (collapseElement) {
-                        let bsCollapse = bootstrap.Collapse.getInstance(collapseElement);
-
-                        if (bsCollapse) {
-                            bsCollapse.hide();
-                        } else {
-                            new bootstrap.Collapse(collapseElement).hide();
-                        }
-                    }
-                }, 1000);
-            },
-            error: function (response) {
-                const toastEl = document.getElementById('liveToast');
-                const liveToast = new bootstrap.Toast(toastEl, {});
-
-                $(".toast-body").html(response.responseJSON);
-
-                liveToast.show();
-            }
-        });
-    });
+    setupDeviceControls();
 });
+
+function setupDeviceControls() {
+    const controlButtons = $('#cover-control button[data-role], #garage-control button[data-role]');
+
+    controlButtons.on('click', function () {
+        const clickedButton = $(this);
+        const collapseContainer = clickedButton.closest('.accordion-collapse');
+        const containerId = collapseContainer.attr('id');
+        const direction = clickedButton.data('role');
+
+        let apiUrl;
+
+        if (containerId === 'cover-control') {
+            apiUrl = '/cover/open-close';
+        } else if (containerId === 'garage-control') {
+            apiUrl = '/garage/move';
+        }
+
+        if (apiUrl) {
+            $.ajax({
+                type: "PATCH",
+                url: apiUrl,
+                data: {"direction": direction},
+                success: function () {
+                    setTimeout(function () {
+                        const collapseElement = collapseContainer[0];
+                        if (collapseElement) {
+                            let bsCollapse = bootstrap.Collapse.getInstance(collapseElement);
+
+                            if (bsCollapse) {
+                                bsCollapse.hide();
+                            } else {
+                                // Awaryjne ukrycie, jeśli instancja nie została znaleziona
+                                new bootstrap.Collapse(collapseElement).hide();
+                            }
+                        }
+                    }, 1000);
+                },
+                error: function (response) {
+                    const toastEl = document.getElementById('liveToast');
+                    if (toastEl && response.responseJSON) {
+                        const liveToast = new bootstrap.Toast(toastEl, {});
+                        $(".toast-body").html(response.responseJSON);
+                        liveToast.show();
+                    }
+                }
+            });
+        } else {
+            console.error('Nie można było ustalić adresu API dla kontenera o ID:', containerId);
+        }
+    });
+}
 
 function onShowGateControllers()
 {
