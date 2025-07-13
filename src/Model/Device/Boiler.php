eea6301bc0416988e314569c7954b2081ec603b2
miko <lukasz@mikowski.pl>
2025-07-13 10:02:34 +0200

    est. fuel consume

--- src/Model/Device/Boiler.php
+++ src/Model/Device/Boiler.php
@@ -4,7 +4,8 @@ namespace App\Model\Device;
 
 final class Boiler extends Device implements DeviceInterface
 {
-    public const NAME           = 'piec';
-    public const DEVICE_ID      = '5432046b0fd8';
-    public const BOUNDARY_POWER = 12;
+    public const NAME             = 'piec';
+    public const DEVICE_ID        = '5432046b0fd8';
+    public const BOUNDARY_POWER   = 12;
+    public const EST_FUEL_CONSUME = 0.024;
 }
