d2c5f2401d597b5680636229c6c38286aa6bf088
miko <lukasz@mikowski.pl>
2025-07-14 15:53:19 +0200

    update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 if git pull -f | grep -q "Already up to date."; then
-  echo "Repo aktualne!"
+  echo "Repozytorium jest już aktualne!"
   exit 0
 fi
 
+echo "Repozytorium zaktualizowane."
+sleep 1
+
+echo "Aktualizacja schematu bazy danych..."
 php bin/console d:s:u --dump-sql --force
+sleep 1
+
+echo "Czyszczenie pamięci podręcznej..."
 php bin/console cache:clear
+sleep 1
+
+echo "Gotowe!"
