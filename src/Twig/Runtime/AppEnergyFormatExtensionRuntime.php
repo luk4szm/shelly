dfb54b0c8f960f969b73ef0557e9b2fac2a0fc7f
miko <lukasz@mikowski.pl>
2025-07-20 23:34:26 +0200

    format kWh

--- src/Twig/Runtime/AppEnergyFormatExtensionRuntime.php
+++ src/Twig/Runtime/AppEnergyFormatExtensionRuntime.php
@@ -6,8 +6,15 @@ use Twig\Extension\RuntimeExtensionInterface;
 
 class AppEnergyFormatExtensionRuntime implements RuntimeExtensionInterface
 {
+    private const KWH_CONVERSION_THRESHOLD = 1000;
+
     public function formatEnergy(float $energy, string $unit = 'Wh'): string
     {
+        if ($energy >= self::KWH_CONVERSION_THRESHOLD) {
+            $energy /= self::KWH_CONVERSION_THRESHOLD;
+            $unit = 'kWh';
+        }
+
         $decimals = match (true) {
             $energy == 0, $energy >= 100 => 0,
             $energy >= 10                => 1,
