11f012b6f10677b50effa3a18122a55707cd1c40
miko <lukasz@mikowski.pl>
2025-08-06 23:48:48 +0200

    clean up dashboard

--- src/Controller/Front/DashboardController.php
+++ src/Controller/Front/DashboardController.php
@@ -3,15 +3,12 @@
 namespace App\Controller\Front;
 
 use App\Entity\DeviceDailyStats;
-use App\Form\GasMeterIndicationType;
-use App\Repository\GasMeterRepository;
 use App\Repository\HookRepository;
 use App\Service\DailyStats\DailyStatsCalculatorInterface;
 use App\Service\DeviceStatus\DeviceStatusHelperInterface;
 use App\Service\Location\LocationFinder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,22 +16,12 @@ final class DashboardController extends AbstractController
 {
     #[Route('/', name: 'app_front_dashboard')]
     public function index(
-        #[AutowireIterator('app.shelly.device_status_helper')]
-        iterable           $statusHelpers,
-        #[AutowireIterator('app.shelly.daily_stats')]
-        iterable           $dailyStatsCalculators,
-        Request            $request,
-        LocationFinder     $locationFinder,
-        GasMeterRepository $gasMeterRepository,
-        HookRepository     $hookRepository,
-    ): Response
-    {
-        $lastGasMeterIndication = $gasMeterRepository->findLast();
-        $locations              = array_merge($locationFinder->getLocations('buffer'), $locationFinder->getLocations('rooms'));
-
-        $gasMeterForm = $this->createForm(GasMeterIndicationType::class, options: [
-            'lastIndication' => $lastGasMeterIndication->getIndication(),
-        ]);
+        #[AutowireIterator('app.shelly.device_status_helper')] iterable $statusHelpers,
+        #[AutowireIterator('app.shelly.daily_stats')] iterable          $dailyStatsCalculators,
+        LocationFinder                                                  $locationFinder,
+        HookRepository                                                  $hookRepository,
+    ): Response {
+        $locations = array_merge($locationFinder->getLocations('buffer'), $locationFinder->getLocations('rooms'));
 
         /** @var DeviceStatusHelperInterface $helper */
         foreach ($statusHelpers as $helper) {
@@ -59,17 +46,11 @@ final class DashboardController extends AbstractController
             ];
         }
 
-        $template = $request->get('template') === 'old'
-            ? 'front/dashboard/index_old.html.twig'
-            : 'front/dashboard/index.html.twig';
-
-        return $this->render($template, [
-            'devices'                => $devices ?? [],
-            'locations'              => $locations,
-            'gasMeterForm'           => $gasMeterForm->createView(),
-            'lastGasMeterIndication' => $lastGasMeterIndication,
-            'temperatures'           => array_values($hookRepository->findActualTemps($locations)),
-            'humidity'               => array_values($hookRepository->findActualHumidity($locations)),
+        return $this->render('front/dashboard/index.html.twig', [
+            'devices'      => $devices ?? [],
+            'locations'    => $locations,
+            'temperatures' => array_values($hookRepository->findActualTemps($locations)),
+            'humidity'     => array_values($hookRepository->findActualHumidity($locations)),
         ]);
     }
 }
