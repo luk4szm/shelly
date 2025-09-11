921faae8ee135e7d107f8a11f30c4fef5e7edb28
miko <lukasz@mikowski.pl>
2025-09-11 22:12:06 +0200

    more rooms

---
+++ src/Model/Location/Bedroom.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Location;
+
+final class Bedroom extends Location implements LocationInterface
+{
+    public const NAME      = 'sypialnia';
+    public const DEVICE_ID = 'dcda0cb72d50';
+    public const GROUP     = ['rooms'];
+}
---
+++ src/Model/Location/BoyRoom.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Location;
+
+final class BoyRoom extends Location implements LocationInterface
+{
+    public const NAME      = 'stasiu';
+    public const DEVICE_ID = 'e4b3233129e0';
+    public const GROUP     = ['rooms'];
+}
