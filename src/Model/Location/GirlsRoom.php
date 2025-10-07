0c3fc5090c64bc18c98badc7252f9d3158c79f29
miko <lukasz@mikowski.pl>
2025-10-07 20:35:30 +0200

    girls room h&t location

---
+++ src/Model/Location/GirlsRoom.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Location;
+
+final class GirlsRoom extends Location implements LocationInterface
+{
+    public const NAME      = 'dziewczynki';
+    public const DEVICE_ID = '8cbfea9bedac';
+    public const GROUP     = ['rooms'];
+}
