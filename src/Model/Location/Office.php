98f7172ebe5545cb4d6bea1972557da91db3bef1
miko <lukasz@mikowski.pl>
2025-10-28 23:49:30 +0100

    adjust location card styling and introduce Office model implementation

---
+++ src/Model/Location/Office.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Location;
+
+final class Office extends Location implements LocationInterface
+{
+    public const NAME      = 'biuro';
+    public const DEVICE_ID = 'dcda0cb76520';
+    public const GROUP     = ['rooms'];
+}
