3a8c31e90e555bcc9461012be3cbb3bb0205d827
miko <lukasz@mikowski.pl>
2025-11-20 23:44:17 +0100

    update boiler fuel consumption estimate

--- src/Model/Device/Boiler.php
+++ src/Model/Device/Boiler.php
@@ -8,5 +8,5 @@ final class Boiler extends Device implements DeviceInterface
     public const DEVICE_ID         = '5432046b0fd8';
     public const BOUNDARY_POWER    = 12;
     public const INSTALLATION_DATE = '2025-02-16';
-    public const EST_FUEL_CONSUME  = 0.024;
+    public const EST_FUEL_CONSUME  = 0.0282;
 }
