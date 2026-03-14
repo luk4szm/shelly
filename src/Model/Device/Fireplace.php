07db7745784ab7bd68049de547bd35d8c6f07cd4
miko <lukasz@mikowski.pl>
2026-03-14 11:12:10 +0100

    new kominek device id

--- src/Model/Device/Fireplace.php
+++ src/Model/Device/Fireplace.php
@@ -5,7 +5,7 @@ namespace App\Model\Device;
 final class Fireplace extends Device implements DeviceInterface
 {
     public const NAME              = 'kominek';
-    public const DEVICE_ID         = '543204705c18';
+    public const DEVICE_ID         = 'CC7B5C8378B4';
     public const BOUNDARY_POWER    = 15;
     public const INSTALLATION_DATE = '2025-03-02';
 }
