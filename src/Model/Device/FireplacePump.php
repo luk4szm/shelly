0595509097c6add28227bbc2f017cb91edaf6be2
miko <lukasz@mikowski.pl>
2026-03-14 11:13:11 +0100

    new fireplace pump device id

--- src/Model/Device/FireplacePump.php
+++ src/Model/Device/FireplacePump.php
@@ -5,7 +5,7 @@ namespace App\Model\Device;
 final class FireplacePump extends Device implements DeviceInterface
 {
     public const NAME              = 'pompa-kominek';
-    public const DEVICE_ID         = 'b0a7324d5034';
+    public const DEVICE_ID         = 'cc7b5c8378b4';
     public const CHANNEL           = 0;
     public const BOUNDARY_POWER    = 10; // Watts
     public const INSTALLATION_DATE = '2026-01-17';
