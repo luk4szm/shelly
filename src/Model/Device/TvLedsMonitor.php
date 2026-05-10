11154b72b3fbce489478be57ca7f4d1c1dcfffd0
miko <lukasz@mikowski.pl>
2026-05-11 00:15:16 +0200

    dmm

--- src/Model/Device/TvLedsMonitor.php
+++ src/Model/Device/TvLedsMonitor.php
@@ -4,7 +4,7 @@ namespace App\Model\Device;
 
 final class TvLedsMonitor extends LightDevice implements DeviceInterface
 {
-    public const NAME      = 'tv_leds_board';
+    public const NAME      = 'tv_leds_monitor';
     public const TYPE      = 'white';
     public const DEVICE_ID = 'ecc9ff4dc3f4';
     public const CHANNEL   = 2;
