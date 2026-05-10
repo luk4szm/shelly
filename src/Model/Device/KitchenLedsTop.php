0c247d877b3dee27a5f13ae80e05feaf225b0780
miko <lukasz@mikowski.pl>
2026-05-11 00:15:24 +0200

    add KitchenLedsTop and KitchenLedsBottom device models

---
+++ src/Model/Device/KitchenLedsTop.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Device;
+
+final class KitchenLedsTop extends LightDevice implements DeviceInterface
+{
+    public const NAME      = 'kitchen_leds_top';
+    public const TYPE      = 'white';
+    public const DEVICE_ID = '2cbcbbc16fa4';
+    public const CHANNEL   = 0;
+}
