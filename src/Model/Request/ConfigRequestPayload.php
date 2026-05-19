044dc8feee1e93787fc27dbdd697966869c26735
miko <lukasz@mikowski.pl>
2026-05-20 01:03:59 +0200

    remove deprecations

--- src/Model/Request/ConfigRequestPayload.php
+++ src/Model/Request/ConfigRequestPayload.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 final readonly class ConfigRequestPayload
 {
     public function __construct(
-        #[Assert\Choice(['occupancy_mode', 'daylight_mode', 'auto_light_inside', 'auto_light_outside'])]
+        #[Assert\Choice(choices: ['occupancy_mode', 'daylight_mode', 'auto_light_inside', 'auto_light_outside'])]
         public ?string $name,
         public mixed   $value,
     ) {
