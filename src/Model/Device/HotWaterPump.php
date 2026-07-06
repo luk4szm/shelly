c40fadb1f3e992e38171cd48320c8eee96c2b743
miko <lukasz@mikowski.pl>
2026-07-06 23:03:31 +0200

    update HotWaterPump constants for name, ID, and installation date

--- src/Model/Device/HotWaterPump.php
+++ src/Model/Device/HotWaterPump.php
@@ -4,9 +4,9 @@ namespace App\Model\Device;
 
 final class HotWaterPump extends Device implements DeviceInterface
 {
-    public const NAME              = 'pompa-recyrkulacja';
-    public const DEVICE_ID         = 'e4b3233b00ac';
+    public const NAME              = 'pompa-cwu';
+    public const DEVICE_ID         = '64b708097270';
     public const CHANNEL           = 0;
     public const BOUNDARY_POWER    = 5;
-    public const INSTALLATION_DATE = '2026-06-28';
+    public const INSTALLATION_DATE = '2026-07-06';
 }
